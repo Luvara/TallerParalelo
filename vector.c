d88c892de34dff30b5dc7b917948cc57ec3184d7
Luis Vargas <130877051+Luvara@users.noreply.github.com>
2024-05-09 11:27:43 -0600

    Update vector.c

--- vector.c
+++ vector.c
@@ -36,20 +36,38 @@ float sum_sqrt(float *a, int N){
 }
 
 float vec_sum_sqrt(float *a, int N){
+  int i;
+  __m256 v;
+  __m256 w;
+  float s;
+  for (int i = 0; i < N; i+=8){
+    v = __m256_load_ps(&a[i]);
+    w = _mm256_add_ps(w, mm256_sqrt_ps(v));
+  }
+  s = 0;
+  for (int i = 0; i < N; i++) 
+    s = s + w[i];
 
-
+  return s;
 }
 
 int main() {
     double t0,t1,t;
     int N;
-    N = 100000;
+    N = 10000000;
     float  *U = malloc(N*sizeof(float));
     init(U,N);
     
     float s;
     t0 = now();
     s = sum_sqrt(U, N);
+    t1 = now();
+    t = t1-t0;
+    printf("The sum of the square root of the %d elements of U equals to %f\n", N, s);
+    printf("The computation was done in %f second(s)\n", t);
+
+    t0 = now();
+    s = vec_sum_sqrt(U, N);
     t1 = now();
     t = t1-t0;
     printf("The sum of the square root of the %d elements of U equals to %f\n", N, s);
